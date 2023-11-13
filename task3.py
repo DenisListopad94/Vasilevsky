402830c3fedd3b6fc326385969e6ec3d5d76ae5d
МаксимСынок <fedenka123@mail.ru>
2023-11-13 20:42:09 +0300

    task3.py

---
+++ task3.py
@@ -0,0 +1,34 @@
+name = "Maksim"
+print(name[2])
+print(name[4])
+print(name[0:5:1])
+print(name[:-2])
+print(name[2:6:2])
+print(name[1:6:2])
+print(name[::-1])
+print(name[::-2])
+print(len(name))
+#
+word = "apple"
+print(word[0] == word[-1])
+#
+animal = "Horse"
+print(animal[2:5:1])
+#
+word = "яблоко"
+print(word[3:6:1])
+print(word[1:5:1])
+#
+x = 'Ivanou Ivan'
+x = x.replace('Ivanou Ivan', ' Ivan Ivanou')
+print(x)
+#
+clubs = ['Madrid' , 'Barca' , 'MU' , 'Milan']
+print(clubs[1])
+#
+str_any = 'asdfghfre'
+print(str_any.index('f',4))
+#
+str1 = 'employment'
+st2 = str1.find('employ')
+print(st2)
